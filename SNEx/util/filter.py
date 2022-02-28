5654a4cf9622a697046accfd51861bcb1ff2d5f2
Anthony Burrow <anthony.r.burrow@gmail.com>
2022-02-28 15:54:05 -0600

    add boxcar filtering method

--- SNEx/util/filter.py
+++ SNEx/util/filter.py
@@ -17,3 +17,25 @@ def monotonic(data, remove_unique=True):
         data_filtered = data_filtered[np.sort(ind)]
 
     return data_filtered
+
+
+def boxcar(data, bins=50):
+    wave = data[:, 0]
+    new_wave_endpoints = np.linspace(wave[0], wave[-1], bins + 1)
+
+    new_data = np.zeros((bins, 3))
+    new_data[:, 0] = 0.5 * (new_wave_endpoints[:-1] + new_wave_endpoints[1:])
+
+    for i in range(bins):
+        wave_mask = (new_wave_endpoints[i] < wave) & \
+                    (wave < new_wave_endpoints[i + 1])
+
+        bin_data = data[wave_mask]
+        new_data[i, 1] = bin_data[:, 1].mean()
+
+        bin_flux_var = bin_data[:, 1].var()
+        bin_flux_err_var = (bin_data[:, 2]**2).sum()
+
+        new_data[i, 2] = np.sqrt(bin_flux_var + bin_flux_err_var)
+
+    return new_data

3c197b9d3bcdb99c5e98cd12cc419a9eb4fb683e
Anthony Burrow <anthony.r.burrow@gmail.com>
2022-03-26 19:38:31 -0500

    cleanup example script

--- example/example.py
+++ example/example.py
@@ -7,19 +7,20 @@ from SNEx import SNEx
 _data_dir = '../ex_data'
 
 fn = f'{_data_dir}/ptf11kly_20110910_MJD55814.43.dat'
+z = 0.001208
 
 wave_range = None
-model = SNEx(fn, wave_range=wave_range)
+model = SNEx(fn, z=z, wave_range=wave_range)
 
-# Check if < 5500 breaks this
-fit_range = (5500., 6500.)
-
-x_pred = np.linspace(4100., 12500., 500)
-
-y_pca, y_err_pca, x_pca = model.predict(regime='NIR', time=0.,
-                                        fit_range=fit_range,
-                                        extrap_method='pca', n_components=8,
-                                        calc_var=True)
+params = {
+    'regime': 'NIR',
+    'time': 0.,
+    'fit_range': (4000., 6500.),
+    'extrap_method': 'pca',
+    'n_components': 15,
+    'calc_var': True
+}
+y_pca, y_err_pca, x_pca = model.predict(**params)
 
 fig, ax = plt.subplots()
 
@@ -30,15 +31,16 @@ ax.fill_between(model.data[:, 0], model.data[:, 1] - model.data[:, 2],
 ax.plot(x_pca, y_pca, 'r-', label='pca')
 ax.fill_between(x_pca, y_pca - y_err_pca, y_pca + y_err_pca, color='#ff7d7d')
 
-# ax.plot(x_pred, y_planck, 'b-', label='planck')
+for i in range(2):
+    wave = params['fit_range'][i]
+    endpoint_ind = abs(model.data[:, 0] - wave).argmin()
+    flux = model.data[:, 1][endpoint_ind]
+    ax.vlines(wave, 0., flux, linestyles='dashed')
 
-ax.set_xlim(3000, 9000)
+ax.set_xlim(3500, 9200)
 ax.set_ylim(1.e-14, 1.2e-12)
 
-# ax.axvline(fit_range[0], 0, 0.6)
-ax.axvline(fit_range[1], 0, 0.6)
-
-ax.set_yscale('log')
+# ax.set_yscale('log')
 
 ax.legend()
 

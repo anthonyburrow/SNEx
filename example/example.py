863a1c88ec831ecc73f32bc1a8abcf948a9a33c5
Anthony Burrow <anthony.r.burrow@gmail.com>
2022-04-12 10:15:29 -0500

    update exampl visuals and add Si II 5972

--- example/example.py
+++ example/example.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
 from SNEx import SNEx
+from SNEx.util.feature_ranges import feature_ranges
 
 
 _data_dir = '../ex_data'
@@ -19,9 +21,9 @@ params = {
     'regime': 'nir',
     'time': 0.,
     'extrap_method': 'pca',
-    'predict_range': (5500., 8000.),
+    'predict_range': (5500., 8500.),
     # 'predict_features': ['Si II 6355'],
-    'fit_features': ('Si II 6355', ),
+    'fit_features': ('Si II 5972', 'Si II 6355'),
     # 'fit_range': (5500., 7000.),
     'n_components': 10,
     'calc_var': True
@@ -40,25 +42,30 @@ y_pca, y_err_pca, x_pca = model.predict(**params)
 
 fig, ax = plt.subplots()
 
-ax.plot(model.data[:, 0], model.data[:, 1], 'k-', label='data')
+ax.plot(model.data[:, 0], model.data[:, 1], 'k-', label='data', zorder=6)
 ax.fill_between(model.data[:, 0], model.data[:, 1] - model.data[:, 2],
-                model.data[:, 1] + model.data[:, 2], color='#595959')
+                model.data[:, 1] + model.data[:, 2], color='#595959',
+                zorder=4)
 
-ax.plot(x_pca, y_pca, 'r-', label='pca')
-ax.fill_between(x_pca, y_pca - y_err_pca, y_pca + y_err_pca, color='#ff7d7d')
+ax.plot(x_pca, y_pca, 'r-', label='pca', zorder=7)
+ax.fill_between(x_pca, y_pca - y_err_pca, y_pca + y_err_pca, color='#ff7d7d',
+                zorder=5)
 
 # ax.plot(x_planck, y_planck, 'b-', label='planck')
 
-# for i in range(2):
-#     wave = params['fit_range'][i]
-#     endpoint_ind = abs(model.data[:, 0] - wave).argmin()
-#     flux = model.data[:, 1][endpoint_ind]
-#     ax.vlines(wave, 0., flux, linestyles='dashed')
+for feature in params['fit_features']:
+    wave_range = feature_ranges[feature]
+    ax.axvspan(*wave_range, alpha=0.3)
+    ax.axvline(wave_range[0], color='k', ls='--', lw=0.8)
+    ax.axvline(wave_range[1], color='k', ls='--', lw=0.8)
 
 ax.set_xlim(3500, 9200)
 ax.set_ylim(2.e-14, 1.2e-12)
 
+ax.xaxis.set_minor_locator(MultipleLocator(250))
+
 # ax.set_yscale('log')
+ax.xaxis.grid(which='both', zorder=0)
 
 ax.legend()
 

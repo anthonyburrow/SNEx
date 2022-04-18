7f84fb53a19fb0eda58d097f8e23d5c00f5f5cdd
Anthony Burrow <anthony.r.burrow@gmail.com>
2022-04-18 18:35:22 -0500

    change to eigenvector plot

--- SNEx/util/pcaplot.py
+++ SNEx/util/pcaplot.py
@@ -7,9 +7,16 @@ def plot_eigenvectors(pcamodel):
     wave = pcamodel.wave
     eig = pcamodel.eigenvectors
 
-    for i in range(4):
-        ax.plot(wave, eig[i], '-', label=f'PC{i + 1}')
+    n_comp = 4
+    for i in range(n_comp):
+        ax.plot(wave, eig[i], '-', label=f'PC{i + 1}', zorder=n_comp - i)
 
+    ax.set_xlabel('Rest wavelength (A)')
+    ax.set_ylabel(r'$F - F_\mu$')
+
+    ax.legend()
+
+    plt.tight_layout()
     fn = './eigenvectors.pdf'
     fig.savefig(fn)
 

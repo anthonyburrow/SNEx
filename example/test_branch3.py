8014d9a417a3b504033d247d34665b326cd51880
Anthony Burrow <anthony.r.burrow@gmail.com>
2022-06-27 14:30:10 -0500

    plot PC correlation matrix

--- example/test_branch3.py
+++ example/test_branch3.py
@@ -47,27 +47,49 @@ def choose_color(prob):
     return color / 255.
 
 
-fig, ax = plt.subplots()
-
 colors = [choose_color(prob) for prob in probs]
-ax.scatter(pc[:, 0], pc[:, 1], s=25., c=colors)
 
-ax.set_xlabel('PC 1')
-ax.set_ylabel('PC 2')
 
-xlim = ax.get_xlim()
-ylim = ax.get_ylim()
+n_pcs = 4
+fig, ax = plt.subplots(n_pcs - 1, n_pcs - 1)
+
+
+def make_plt(_ax, pc1, pc2):
+    if pc1 >= pc2:
+        _ax.get_xaxis().set_ticks([])
+        _ax.get_yaxis().set_ticks([])
+        _ax.spines['top'].set_visible(False)
+        _ax.spines['right'].set_visible(False)
+        _ax.spines['bottom'].set_visible(False)
+        _ax.spines['left'].set_visible(False)
+        return
+
+    _ax.scatter(pc[:, pc1], pc[:, pc2], s=10., c=colors)
+    _ax.tick_params(axis='both', labelsize=6., length=3.)
+
+    # _ax.set_xlabel(f'PC {pc1 + 1}')
+    # _ax.set_ylabel(f'PC {pc2 + 1}')
+
+
+for j in range(1, n_pcs):
+    for i in range(0, n_pcs - 1):
+        make_plt(ax[j - 1, i], i, n_pcs - j)
+
+[ax[i - 1, 0].set_ylabel(f'PC {n_pcs - i + 1}') for i in range(1, n_pcs)]
+[ax[-1, i].set_xlabel(f'PC {i + 1}') for i in range(0, n_pcs - 1)]
+
+# xlim = _ax.get_xlim()
+# ylim = _ax.get_ylim()
 
 for i in range(4):
     col = np.array(color_map[i]) / 255.
-    ax.scatter([-1.], [-1.], s=25., c=[col], label=name_map[i])
-
-ax.set_xlim(xlim)
-ax.set_ylim(ylim)
-
-ax.legend()
+    ax[-2, -1].scatter([-1.], [-1.], s=25., c=[col], label=name_map[i])
+ax[-2, -1].set_xlim(1., 2.)
+ax[-2, -1].set_ylim(1., 2.)
+ax[-2, -1].legend(loc='upper left')
 
 plt.tight_layout()
+plt.subplots_adjust(wspace=0.20, hspace=0.2)
 
 fn = 'branch_pc.pdf'
 fig.savefig(fn)

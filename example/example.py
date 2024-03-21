12f1739c0f7e1430e44b068732b79cccbfebabeb
Anthony Burrow <anthony.r.burrow@gmail.com>
2024-03-21 10:55:06 -0500

    update example

--- example/example.py
+++ example/example.py
@@ -17,17 +17,13 @@ read_params = {
 model = SNEx(fn, **read_params)
 
 params = {
-    'regime': 'nir',
     'time': 0.05,
     'extrap_method': 'pca',
-    'fit_range': (5700., 8400.),
+    'fit_range': (5000., 8400.),
     'predict_range': (8400., 23000.),
-    # 'fit_features': ('Si II 5972', 'Si II 6355'),
-    # 'predict_features': ['O I', 'Ca II NIR'],
-    'n_components': 8,
+    'n_components': 10,
     'calc_var': True,
     'plot_pca': True,
-    'norm_method': 'mean'
 }
 y_pca, y_err_pca, x_pca = model.predict(**params)
 
@@ -38,58 +34,23 @@ ax.set_xlim(0.3500, 2.3000)
 # ax.set_ylim(7e-15, 1.2e-12)
 # ax.set_ylim(0., 1.2e-12)
 
-ax.plot(model.data[:, 0] / 1e4, model.data[:, 1], 'k-', label='data', zorder=6)
-ax.fill_between(model.data[:, 0] / 1e4, model.data[:, 1] - model.data[:, 2],
+ax.plot(model.data[:, 0] * 1e-4, model.data[:, 1], 'k-', label='Observed',
+        zorder=6)
+ax.fill_between(model.data[:, 0] * 1e-4, model.data[:, 1] - model.data[:, 2],
                 model.data[:, 1] + model.data[:, 2], color='#595959',
                 zorder=4)
 
+ax.plot(x_pca * 1e-4, y_pca, 'r-', zorder=7)
+ax.fill_between(x_pca * 1e-4, y_pca - y_err_pca, y_pca + y_err_pca,
+                color='#ff7d7d', zorder=5)
 
-def plot_feature(axis, feature):
-    wave_range = feature_ranges[feature]
-    mask = (wave_range[0] <= x_pca) & (x_pca <= wave_range[1])
+# Show fit range
+ax.axvline(params['fit_range'][0] * 1e-4, color='r', ls='--', lw=0.8, zorder=10)
+ax.axvline(params['fit_range'][1] * 1e-4, color='r', ls='--', lw=0.8, zorder=10)
 
-    x = x_pca[mask]
-    y = y_pca[mask]
-    y_err = y_err_pca[mask]
-    axis.plot(x, y, 'r-', zorder=7)
-    axis.fill_between(x, y - y_err, y + y_err, color='#ff7d7d', zorder=5)
-
-    axis.axvspan(*wave_range, alpha=0.3)
-    axis.axvline(wave_range[0], color='k', ls='--', lw=0.8, zorder=10)
-    axis.axvline(wave_range[1], color='k', ls='--', lw=0.8, zorder=10)
-
-    x_pos, y_pos = wave_range[0] + 30., 0.018 * axis.get_ylim()[1]
-    axis.text(x_pos, y_pos, feature, rotation=90., fontsize=8.)
-
-
-def plot_range(axis, wave_range):
-    _x_pca = x_pca / 1e4
-    _wave_range = (wave_range[0] / 1e4, wave_range[1] / 1e4)
-
-    mask = (_wave_range[0] <= _x_pca) & (_x_pca <= _wave_range[1])
-
-    x = _x_pca[mask]
-    y = y_pca[mask]
-    y_err = y_err_pca[mask]
-    axis.plot(x, y, 'r-', zorder=7)
-    axis.fill_between(x, y - y_err, y + y_err, color='#ff7d7d', zorder=5)
-
-    axis.axvspan(*_wave_range, alpha=0.3, zorder=2)
-    axis.axvline(_wave_range[0], color='k', ls='--', lw=0.8, zorder=10)
-    axis.axvline(_wave_range[1], color='k', ls='--', lw=0.8, zorder=10)
-
-
-try:
-    for feature in params['fit_features']:
-        plot_feature(ax, feature)
-except KeyError:
-    plot_range(ax, params['fit_range'])
-
-try:
-    for feature in params['predict_features']:
-        plot_feature(ax, feature)
-except KeyError:
-    plot_range(ax, params['predict_range'])
+# Tellurics
+ax.axvspan(1.2963, 1.4419, alpha=0.8, color='grey', zorder=3)
+ax.axvspan(1.7421, 1.9322, alpha=0.8, color='grey', zorder=3)
 
 ax.xaxis.set_minor_locator(MultipleLocator(0.0500))
 
@@ -98,9 +59,6 @@ ax.xaxis.grid(which='both', zorder=4)
 
 ax.set_xlabel('Rest wavelength [$\mu m$]')
 ax.set_ylabel('Flux')
-
-ax.axvspan(1.2963, 1.4419, alpha=0.8, color='grey', zorder=3)
-ax.axvspan(1.7421, 1.9322, alpha=0.8, color='grey', zorder=3)
 
 line = ax.plot([0.], [0.], 'r-', label='PCA reconstruction')
 ax.legend()
